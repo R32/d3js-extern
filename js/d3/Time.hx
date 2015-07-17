9fa46b785677daae10b5faf4c0b530571d0d9f2f
235 <hotup@qq.com>
2015-07-18 01:29:47 +0800

    time::interval

--- js/d3/Time.hx
+++ js/d3/Time.hx
@@ -1,6 +1,8 @@
 package js.d3;
 
 import js.d3.time.Format;
+import js.d3.time.Scale;
+import js.d3.time.Interval;
 
 extern class Time {
 	
@@ -9,5 +11,75 @@ extern class Time {
 	*/
 	function format(specifier:String):Format;
 	
-
+	/**
+	Constructs a new time scale with the default domain and range; the ticks and tick format are configured for local time. 
+	*/
+	function scale():Scale;
+	
+	/**
+	Returns the specified interval. The following intervals are supported:
+	*/
+	var second(default, null):Interval;
+	var seconds(default, null):Intervals;
+	
+	var minute(default, null):Interval;
+	var minutes(default, null):Intervals;
+	
+	var hour(default, null):Interval;
+	var hours(default, null):Intervals;
+	
+	var day(default, null):Interval;
+	var days(default, null):Intervals;
+	
+	var week(default, null):Interval;
+	var weeks(default, null):Intervals;
+	
+	var sunday(default, null):Interval;
+	var sundays(default, null):Intervals;
+	
+	var monday(default, null):Interval;
+	var mondays(default, null):Intervals;
+	
+	var tuesday(default, null):Interval;
+	var tuesdays(default, null):Intervals;
+	
+	var wednesday(default, null):Interval;
+	var wednesdays(default, null):Intervals;
+	
+	var thursday(default, null):Interval;
+	var thursdays(default, null):Intervals;
+	
+	var friday(default, null):Interval;
+	var fridays(default, null):Intervals;
+	
+	var saturday(default, null):Interval;
+	var saturdays(default, null):Intervals;
+	
+	var month(default, null):Interval;
+	var months(default, null):Intervals;
+	
+	var year(default, null):Interval;
+	var years(default, null):Intervals;
+	
+	
+	/**
+	Returns the day number for the given date. The first day of the year (January 1) is always the 0th day. Unlike the d3.time.format's %j directive, dayOfYear is 0-based rather than 1-based.
+	*/
+	function dayOfYear(d:Date):Int;
+	
+	function weekOfYear(d:Date):Int;
+	
+	function sundayOfYear(d:Date):Int;
+	
+	function mondayOfYear(d:Date):Int;
+	
+	function tuesdayOfYear(d:Date):Int;
+	
+	function wednesdayOfYear(d:Date):Int;
+		
+	function thursdayOfYear(d:Date):Int;
+	
+	function fridayOfYear(d:Date):Int;
+	
+	function saturdayOfYear(d:Date):Int;
 }
---
+++ js/d3/time/Interval.hx
@@ -0,0 +1,80 @@
+package js.d3.time;
+
+/**
+Time intervals are irregular! For example, there are 60 seconds in a minute, but 24 hours in a day. Even more confusing, some days have 23 or 25 hours due to daylight saving time, and the standard Gregorian calendar uses months of differing lengths. And then there are leap years!
+
+To simplify manipulation of and iteration over time intervals, D3 provides a handful of time utilities in addition to the time scale and format. The utilities support both local time and UTC time. Local time is determined by the browser's JavaScript runtime; arbitrary time zone support would be nice, but requires access to the Olson zoneinfo files.
+
+https://github.com/mbostock/d3/wiki/Time-Intervals
+*/
+
+
+extern class Interval extends IntervalUtc{
+	
+	/**
+	Alias for interval.floor(date). For example, d3.time.day(new Date()) returns midnight (12:00 AM) on the current day, in local time. 
+	*/
+	@:selfCall override function selfCall(d:Date):Date;
+	
+	/**
+	IntervalUtc
+	*/
+	var utc(default,null):IntervalUtc;
+}
+
+extern class IntervalUtc {
+
+	/**
+	Returns a corresponding time interval in UTC rather than local time. For example, d3.time.day.range(start, stop) returns local time days between start and stop, while d3.time.day.utc.range(start, stop) returns UTC days between start and stop. 
+	*/
+	@:selfCall function selfCall(d:Date):Date;
+	
+	/**
+	Rounds down the specified date, returning the latest time interval before or equal to date. For example, d3.time.day.floor(new Date()) returns midnight (12:00 AM) on the current day, in local time. 
+	*/
+	function floor(d:Date):Date;
+	
+	/**
+	Rounds up the specified date, returning the earliest time interval after or equal to date. For example, d3.time.day.ceil(new Date()) returns midnight (12:00 AM) on the following day, in local time (unless you happen to run this code at exactly midnight, in which case it returns the current time). 
+	*/
+	function ceil(d:Date):Date;
+	
+	/**
+	Rounds up or down the specified date, returning the closest time interval to date. For example, d3.time.day.round(new Date()) returns midnight (12:00 AM) on the current day if it is on or before noon, and midnight of the following day if it is after noon. 
+	*/
+	function round(d:Date):Date;
+	
+	/**
+	Returns every time interval after or equal to start and before stop. If step is specified, then every step'th interval will be returned, based on the interval number (such as day of month for d3.time.day). For example, a step of 2 will return the 1st, 3rd, 5th etc. of the month with d3.time.day. 
+	
+	zh-cn: 返回 起始至结束之间的 Date 数组, 间隔由不同类型的 step 指定. 如果 day, step = 1 则为 1 天
+	*/
+	function range(start:Date, stop:Date, step:Float = 1):Array<Date>;
+	
+	/**
+	Returns a new date equal to date plus step intervals. If step is negative, then the returned date will be before the specified date; if step is zero, then a copy of the specified date is returned. This method does not round the specified date to the interval. For example, if it is currently 5:34 PM, then d3.time.day.offset(new Date(), 1) returns 5:34 PM tomorrow (even if Daylight Savings Time changes!).
+	*/
+	function offset(date:Date, step:Float):Date;
+}
+
+
+
+extern class Intervals extends IntervalUtcs{
+	
+	/**
+	Alias for Interval.range 
+	*/
+	@:selfCall override function selfCall(start:Date, stop:Date, step:Float = 1):Array<Date>;
+	
+	/**
+	Returns a corresponding time interval in UTC rather than local time. For example, d3.time.day.range(start, stop) returns local time days between start and stop, while d3.time.day.utc.range(start, stop) returns UTC days between start and stop. 
+	*/
+	var utc(default,null):IntervalUtcs;
+}
+
+extern class IntervalUtcs {
+	/**
+	Alias for IntervalUtc.range 
+	*/
+	@:selfCall function selfCall(start:Date, stop:Date, step:Float = 1):Array<Date>;
+}
